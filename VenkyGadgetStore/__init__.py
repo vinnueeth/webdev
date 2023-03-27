f2b478fb516c3e0cb49a8eb3e6027f402f0f2d96
vinnueeth <129020266+vinnueeth@users.noreply.github.com>
2023-03-27 22:52:33 +0100

    Update __init__.py

--- VenkyGadgetStore/__init__.py
+++ VenkyGadgetStore/__init__.py
@@ -11,7 +11,7 @@ DB_NAME = "database.db"
 def create_VenkyGadgetStore_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'my key'
-   # app.config['DATABASE_URL'] = f'postgres://kjtqtxgfanclfu:4825a34ba4737ddebfe6b84402b908c925f8da413f08ae190ebdc9ff01990858@ec2-54-155-110-181.eu-west-1.compute.amazonaws.com:5432/d9jv9qo4dbpeia'
+   # app.config['DATABASE_URL'] = f'postgres://rznsalywifovow:457099221710f6c1530351a2f371129b0c5227db93f4b41a25033f2b623630a8@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/dat2cntmt9lq5f'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
